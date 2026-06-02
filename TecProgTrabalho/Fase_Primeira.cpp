eee051c7928b15279b8efdb428fde10e93c2397a
Ablebleu <ablebleublum@gmail.com>
2026-06-01 22:35:38 -0300

    Versão aleatória para apresentar ao professor

--- TecProgTrabalho/Fase_Primeira.cpp
+++ TecProgTrabalho/Fase_Primeira.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "Fase_Primeira.h"
 #include "Plataforma.h"
 #include "Alien.h"
@@ -30,17 +31,25 @@ void Fase_Primeira::criar() {
 
 
 void Fase_Primeira::criarInimFaceis() {
-	Alien* pAlien1 = new Alien(sf::Vector2f(200.0f, 550.0f), 0, 1.0f);
+	Alien* pAlien1 = new Alien(sf::Vector2f(1200.0f, 550.0f), 0, 1.0f);
 	if (pAlien1) {
 		lista_ents.incluir(static_cast<Entidade*>(pAlien1));
 	}
-	Alien* pAlien2 = new Alien(sf::Vector2f(400.0f, 550.0f), 0, 1.0f);
+	Alien* pAlien2 = new Alien(sf::Vector2f(2200.0f, 550.0f), 0, 1.0f);
 	if (pAlien2) {
 		lista_ents.incluir(static_cast<Entidade*>(pAlien2));
 	}
-	Alien* pAlien3 = new Alien(sf::Vector2f(600.0f, 550.0f), 0, 1.0f);
+	Alien* pAlien3 = new Alien(sf::Vector2f(3000.0f, 550.0f), 0, 1.0f);
 	if (pAlien3) {
 		lista_ents.incluir(static_cast<Entidade*>(pAlien3));
+	}
+	for (int i = 0; i < 50; i++) {
+		if (std::rand() % 100 < 25) {
+			Alien* pAlien = new Alien(sf::Vector2f(1200.0f+i*150.f, 550.0f), 0, 1.0f);
+			if (pAlien) {
+				lista_ents.incluir(static_cast<Entidade*>(pAlien));
+			}
+		}
 	}
 }
 
@@ -57,17 +66,26 @@ void Fase_Primeira::criarInimigos() {
 }
 
 void Fase_Primeira::criarObstaculo() {
-	Plataforma* pPlat1 = new Plataforma(sf::Vector2f(100.0f, 100.0f), sf::Vector2f(2.0f, 1.0f));
+	Plataforma* pPlat1 = new Plataforma(sf::Vector2f(1200.0f, 300.0f), sf::Vector2f(2.0f, 1.0f));
 	if (pPlat1) {
 		lista_ents.incluir(static_cast<Entidade*>(pPlat1));
 	}
-	Plataforma* pPlat2 = new Plataforma(sf::Vector2f(400.0f, 300.0f), sf::Vector2f(2.0f, 1.0f));
+	Plataforma* pPlat2 = new Plataforma(sf::Vector2f(2000.0f, 450.0f), sf::Vector2f(2.0f, 1.0f));
 	if (pPlat2) {
 		lista_ents.incluir(static_cast<Entidade*>(pPlat2));
 	}
-	Plataforma* pPlat3 = new Plataforma(sf::Vector2f(700.0f, 100.0f), sf::Vector2f(2.0f, 1.0f));
+	Plataforma* pPlat3 = new Plataforma(sf::Vector2f(3200.0f, 300.0f), sf::Vector2f(2.0f, 1.0f));
 	if (pPlat3) {
 		lista_ents.incluir(static_cast<Entidade*>(pPlat3));
+	}
+	for (int i = 0; i < 30; i++) {
+		if (std::rand() % 100 < 10) {
+			Plataforma* pPlat3 = new Plataforma(sf::Vector2f(800.0f + i*200.f, 300.0f + std::rand()%250 * 1.0f), 
+												sf::Vector2f(2.0f+rand()%2 * 1.f - 1.0f, 1.0f + std::rand()%2 * 0.5f - 0.5f));
+			if (pPlat3) {
+				lista_ents.incluir(static_cast<Entidade*>(pPlat3));
+			}
+		}
 	}
 }
 void Fase_Primeira::criarCenario() {
